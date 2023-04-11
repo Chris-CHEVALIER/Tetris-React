2d597687125b67cfb56f57d506502c57c97be657
Chris <chevalier@chris-freelance.com>
2023-04-11 15:21:03 +0200

    creating tetrominos

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Cell from './Cell'
 
-export default function Stage({stage}) {
+export default function Stage ({ stage }) {
   return (
-    <div><Cell /></div>
+    <div>
+      {stage.map(row => row.map((cell, i) => <Cell key={i} type={cell[0]} />))}
+    </div>
   )
 }
